aa526cd0c1726b4aedc592daa0cf6037f2558cba
Peter Choi <phc284@gmail.com>
2018-02-12 18:04:46 -0600

    remove sensitive info

--- models/teamModel.js
+++ models/teamModel.js
@@ -1,8 +1,7 @@
 import $ from 'jquery';
 
 const STAT_URL = 'https://api.mysportsfeeds.com/v1.1/pull/nfl/2016-2017-regular/overall_team_standings.json?teamstats=W,L,T,PF,PA';
-const username = "phc284";
-const password = "gotrunks8";
+
 
 export const getTeams = function(callback) {
   console.log(btoa({username} + ":" + {password}));
